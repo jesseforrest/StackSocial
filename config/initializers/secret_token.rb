0da226cc7c7e6fbda88c8f835579ae91a8132615
Jesse Forrest <jesseforrest@gmail.com>
2013-08-24 16:02:03 -0700

    Initial release

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StackSocial::Application.config.secret_key_base = '07509208e8b5bc97591506294d2552d825bc2f08b3eb718a4ed59bc529b95aeb3c75030bfccdb22375ae4fc4db600fad4f70d87f0f37abe6f2c4fdab7e1bc21e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StackSocial::Application.config.session_store :cookie_store, key: '_StackSocial_session'
