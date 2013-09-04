9489a9029d099c3940c758786a79d37e6f2abeec
Jesse Forrest <jesseforrest@gmail.com>
2013-09-04 14:23:42 -0700

    Added additional frontend functionality

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,20 @@
+/**
+ * This Javascript file is used to add functionality to the home page.
+ * 
+ * @author Jesse Forrest <jesse@jesseforrest@gmail.com> 
+ * @package StackSocial
+ * @copyright Copyright (c) 2013
+ * @filesource 
+ */
+
+/*jslint browser: true, white: true, maxerr: 50, indent: 4, nomen: true, sub: true, */
+/*global $: false */
+/**
+ * On document ready state
+ */ 
+$(document).ready(function() {
+   // If welcome page
+   if ($("#welcome_heading").length > 0) {
+      $("#welcome_heading").fadeIn("slow");
+   }
+});
